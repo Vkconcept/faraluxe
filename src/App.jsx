83710db716c6521fb91a5f644aa86c9b5ec81303
williams victor <williamsvikky76@gmail.com>
2025-11-27 20:02:06 -0800

    Updated App.jsx and main.jsx with React + Tailwind fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route, Navigate } from "react-router-dom";
-import Navbar from "./Components/Navbar"; 
-import Footer from "./Components/Footer"; 
+import Navbar from "./Components/Navbar";
+import Footer from "./Components/Footer";
 import Shop from "./Pages/Shop";
 import About from "./Pages/About";
 import Contact from "./Pages/Contact";
@@ -9,10 +9,8 @@ import { Home } from "./Pages/Home";
 export default function App() {
   return (
     <div className="flex flex-col min-h-screen">
-      {/* Navbar */}
-      <Navbar /> 
+      <Navbar />
 
-      {/* Main content */}
       <div className="flex-grow">
         <Routes>
           <Route path="/" element={<Home />} />
@@ -20,12 +18,10 @@ export default function App() {
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
 
-          {/* ✅ Redirect all unknown routes to Home */}
           <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </div>
 
-      {/* Footer */}
       <Footer />
     </div>
   );
